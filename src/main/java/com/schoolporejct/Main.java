607e2c56ec5bc41c4479c492f9f014d60c7e0c56
rutp6vmp <rutp6vmp@gmail.com>
2022-12-19 17:21:26 +0800

    Initial commit

--- src/main/java/com/schoolporejct/Main.java
+++ src/main/java/com/schoolporejct/Main.java
@@ -4,7 +4,13 @@ package com.schoolporejct;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
+@RestController
+
 public class Main {
     public static void main(String[] args)
     {
@@ -12,5 +18,15 @@ public class Main {
         SpringApplication.run(Main.class,args);
 
     }
+
+    @GetMapping("/greet")
+    public String greet(){
+        return "hello";
+    }
+    @GetMapping("/Resgreet")
+    public GreetResponse Resgreet(){
+        return new GreetResponse("GR");
+    }
+    record GreetResponse(String Resgreet){}
 }
 
