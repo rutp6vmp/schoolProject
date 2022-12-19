db9934565a2b1c1b5a68e54f34b51d99077b92b7
rutp6vmp <rutp6vmp@gmail.com>
2022-12-19 22:53:16 +0800

    Initial commit

--- src/main/java/com/schoolporejct/Main.java
+++ src/main/java/com/schoolporejct/Main.java
@@ -8,9 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Objects;
+
 @SpringBootApplication
 @RestController
-
+@ResponseBody
 public class Main {
     public static void main(String[] args)
     {
@@ -19,14 +22,40 @@ public class Main {
 
     }
 
+//   API_url: http://localhost:8080/greet
     @GetMapping("/greet")
     public String greet(){
-        return "hello";
+        return "just hello";
     }
-    @GetMapping("/Resgreet")
-    public GreetResponse Resgreet(){
-        return new GreetResponse("GR");
+
+//   API_url: http://localhost:8080/FirstAPI
+    @GetMapping("/FirstAPI")
+    public FirstAPIResponse FirstAPI(){
+        return new FirstAPIResponse("first API JSON string!");
     }
-    record GreetResponse(String Resgreet){}
+    record FirstAPIResponse(String FirstAPI){}
+
+//   API_url: http://localhost:8080/Second
+    @GetMapping("/SecondAPI")
+    public SecondAPIResponse SecondAPI(){
+            SecondAPIResponse response =  new SecondAPIResponse(
+                "first API Second string!",
+                List.of("java", "Golan" , "javascript"),
+                new Person("Daniel",28 , 30_300)
+             );
+
+            return response;
+    }
+    record  Person (String name , int age , double savings){
+
+    }
+    record SecondAPIResponse(
+            String SecondAPI,
+            List<String> favProgrammingLanguages,
+            Person person
+    ){}
+
+
+
 }
 
